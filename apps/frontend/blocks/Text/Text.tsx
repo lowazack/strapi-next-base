33ec7d53b1ca2c97c43f184c0ef16f16ff44880e
Jack <loaizajack@gmail.com>
2022-10-19 18:46:15 +0000

    styling footer building text component

--- apps/frontend/blocks/Text/Text.tsx
+++ apps/frontend/blocks/Text/Text.tsx
@@ -1,9 +1,11 @@
-import ReactMarkdown from "react-markdown"
+import ReactMarkdown from "react-markdown";
+import styles from "./Text.module.scss";
+
 export default function Text({data}){
     const {text} = data
 
     return (
-    <div>
+    <div className={styles.text}>
         <ReactMarkdown>{text}</ReactMarkdown>
     </div>
     )
--- apps/frontend/components/Footer/Footer.tsx
+++ apps/frontend/components/Footer/Footer.tsx
@@ -1,13 +1,39 @@
 import MadeBy from "../MadeBy";
 import styles from "./Footer.module.scss";
 
+import Logo from "../../svg/logo.svg"
+import Facebook from "../../svg/facebook.svg"
+import Instagram from "../../svg/instagram.svg"
+import Twitter from "../../svg/twitter.svg"
+import LinkedIn from "../../svg/linkedIn.svg"
+
 export default function Footer(){
     return (
+        <>
         <div className={styles.footer}>
-            <div className={styles.content}>
-                <h1>Footer</h1>
-            </div>
-            <MadeBy />
+            <footer className={styles.content}>
+                <div className={styles.column}>
+                    <Logo className={styles.logo}/>
+
+                    <div className={styles.socials}>
+                        <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                            <Facebook />
+                        </a>
+                        <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                            <Instagram />
+                        </a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+                            <Twitter />
+                        </a>
+                        <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+                            <LinkedIn />
+                        </a>
+                    </div>
+                
+                </div>
+            </footer>
         </div>
+        <MadeBy />
+        </>
     )
 }
